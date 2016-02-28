f8911dbb7ded59b21279b068bdc1d206719f163c
Kai Mou <kaimou1357@gmail.com>
2016-02-28 18:28:41 -0500

    Fixed null error from regex not fast enough

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from bs4 import BeautifulSoup
 import dryscrape
 import json
 import requests
-import threading
 import re
 import csv
-from multiprocessing import Pool
+
 
 
 def main():
@@ -19,6 +18,7 @@ def main():
 	#p = Pool(5)
 	#call_list = p.map(s.returnCallListAsJSON)
 	call_list = s.returnCallListAsJSON()
+	create_csv(call_list);
 
 
 
@@ -28,16 +28,16 @@ def main():
 # .csv file creation
 
 
-def create_csv():
+def create_csv(call_list):
 
 	create_copy = raw_input(str("Would you like to create a copy of the data (Yes/No)?"))
 
 	if create_copy == 'Yes' or 'yes':
 		csvfile = open("test.csv",'wb')
 		csvwriter = csv.writer(csvfile, delimiter = ',')
-		csvwriter.writerows(['Underlying Ticker', 'Strike Price', 'Ask', 'Bid'])
+		csvwriter.writerow(['Underlying Ticker', 'Strike Price', 'Ask', 'Bid'])
 		for option in call_list:
-			csvwriter.writerows([option['contractSymbol'], option['strike']['fmt'], option['ask']['fmt'], option['bid']['fmt']])
+			csvwriter.writerow([option['contractSymbol'], option['strike']['fmt'], option['ask']['fmt'], option['bid']['fmt']])
 		csvfile.close()
 	else:
 		#make sure to use raw_input and not just input().
@@ -46,26 +46,32 @@ def create_csv():
 
 class Scraper(object):
 	def __init__(self, base_url):
-		self.r = requests.get(base_url)
-		self.data = self.r.text
-		self.soup = BeautifulSoup(self.data, 'lxml')
-		self.list = []
-		for n in self.soup.find_all('script'):
-			self.list.append(n)
-
+		self.base_url = base_url
 
 	def returnCallListAsJSON(self):
 		#16th index will give us the correct <script> tags for options_chain data.
-		raw_options_chain = str(self.list.pop(16))
+		while True:
+			try:
+				r = requests.get(self.base_url)
+				data = r.text
+				#print(self.r.text)
+				soup = BeautifulSoup(data, 'lxml')
+				option_list = []
+				for n in soup.find_all('script'):
+					option_list.append(n)
+				raw_options_chain = str(option_list.pop(16))
 
-		startoptions = [a.start() for a in list(re.finditer('calls', raw_options_chain))]
-		endoptions = [a.start() for a in list(re.finditer('_options', raw_options_chain))]
+				startoptions = [a.start() for a in list(re.finditer('calls', raw_options_chain))]
+				endoptions = [a.start() for a in list(re.finditer('_options', raw_options_chain))]
 
-		raw_options_chain = raw_options_chain[startoptions[0]-2:endoptions[0]-2]
+				raw_options_chain = raw_options_chain[startoptions[0]-2:endoptions[0]-2]
 
-		options_json = json.loads(raw_options_chain)
+				options_json = json.loads(raw_options_chain)
 
-		calls_list = options_json['calls']
+				calls_list = options_json['calls']
+				break
+			except IndexError:
+				continue
 
 		return calls_list
 
